ca647ef890a0c68b0989d8334075b4bd6ba5986d
Dmitrii-Pilipenko <dvpilipenko@edu.hse.ru>
2025-11-24 18:41:54 +0300

    1

--- Gomoku/Program.cs
+++ Gomoku/Program.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        GameField field = new GameField();
+        field.InitializeField();
+
+        field.SetValue(2, 5, "X");
+
+        field.DisplayField();
+
+        Console.ReadKey();
+    }
+}
+
+class GameField
+{
+    private string[,] field = new string[15, 15];
+    private string fieldLine = new string('—', 61);
+
+    public void InitializeField()
+    {
+        for (int row = 0; row < 15; row++)
+        {
+            for (int col = 0; col < 15; col++)
+            {
+                field[row, col] = "O";
+            }
+        }
+    }
+
+    public void DisplayField()
+    {
+        Console.WriteLine(fieldLine);
+        for (int row = 0;row < 15; row++)
+        {
+            Console.Write("| ");
+            for (int col = 0;col < 15; col++)
+            {
+                Console.Write(field[row, col] + " " + "|" + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(fieldLine);
+        }
+    }
+
+    public void SetValue(int row, int col, string value)
+    {
+        if (row >= 0 && row < 15 && col >= 0 && col < 15)
+        {
+            field[row, col] = value;
+        }
+        else
+        {
+            Console.WriteLine("Некорректные данные!");
+        }
+    }
+}
